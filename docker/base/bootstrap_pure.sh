cb877fd9d649e65a3348dbd287576c361fdc5f4a
techknowlogick <matti@mdranta.net>
2019-03-15 01:12:55 -0400

    Update bootstrap_pure.sh

--- docker/base/bootstrap_pure.sh
+++ docker/base/bootstrap_pure.sh
@@ -63,11 +63,11 @@ GOOS=darwin GOARCH=386 CGO_ENABLED=1 CC=o32-clang go install std
 
 # Install xgo within the container to enable internal cross compilation
 echo "Installing xgo-in-xgo..."
-go get -u github.com/karalabe/xgo
+go get -u github.com/techknowlogick/xgo
 ln -s /go/bin/xgo /usr/bin/xgo
 
 # Install gomobile tool for android/ios frameworks
-echo "Installing gomobile..."
-go get -u golang.org/x/mobile/cmd/gomobile
-/go/bin/gomobile init -ndk /usr/local/android-ndk-r13b/
-/go/bin/gomobile version
+#echo "Installing gomobile..."
+#go get -u golang.org/x/mobile/cmd/gomobile
+#/go/bin/gomobile init -ndk /usr/local/android-ndk-r13b/
+#/go/bin/gomobile version
