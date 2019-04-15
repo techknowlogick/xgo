9ee0846463de7ea9957cf9fdf2c9ae04ea140fab
techknowlogick <matti@mdranta.net>
2019-04-14 20:48:41 -0400

    use appropriate path (#13)

--- xgo.go
+++ xgo.go
@@ -4,7 +4,7 @@
 // Released under the MIT license.
 
 // Wrapper around the GCO cross compiler docker container.
-package main
+package main // import "src.techknowlogick.com/xgo"
 
 import (
 	"bytes"
