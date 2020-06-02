a09175ea9056926103c523249683c8e35fcf6628
techknowlogick <techknowlogick@gitea.io>
2020-06-02 02:06:27 -0400

    go modules fix (#69)
    * go modules fix
    
    * Update build.sh

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -151,11 +151,11 @@ NAME=`basename $1/$PACK`
 # Go module-based builds error with 'cannot find main module'
 # when $PACK is defined
 if [[ "$USEMODULES" = true ]]; then
-  PACK_RELPATH=""
   NAME=`sed -n 's/module\ \(.*\)/\1/p' /source/go.mod`
-else
-  PACK_RELPATH="./$PACK"
 fi
+
+# Support go module package
+PACK_RELPATH="./$PACK"
 
 if [ "$OUT" != "" ]; then
   NAME=$OUT
