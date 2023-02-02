0de927b4c4db081d0790316804f5ad4ef1ca9ed9
Andrew Thornton <art27@cantab.net>
2023-02-02 07:22:57 +0000

    Stop bootstrapping for go 1.20+
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- docker/toolchain/bootstrap_pure.sh
+++ docker/toolchain/bootstrap_pure.sh
@@ -26,63 +26,70 @@ if [ "$GOROOT" == "" ]; then
 fi
 export GOROOT_BOOTSTRAP=$GOROOT
 
-# Pre-build all guest distributions based on the root distribution
-echo "Bootstrapping linux/386..."
-GOOS=linux GOARCH=386 CGO_ENABLED=1 go install std
+GO_VERSION_MAJOR=$(go version | sed -e 's/.*go\([0-9]\+\)\..*/\1/')
+GO_VERSION_MINOR=$(go version | sed -e 's/.*go[0-9]\+\.\([0-9]\+\)\..*/\1/')
 
-echo "Bootstrapping linux/arm64..."
-GOOS=linux GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc-6 go install std
+if [ "$GO_VERSION_MAJOR" -lt 1 ] || { [ "$GO_VERSION_MAJOR" == 1 ] && [ "$GO_VERSION_MINOR" -lt 20 ]; }; then
+  # Pre-build all guest distributions based on the root distribution
+  echo "Bootstrapping linux/386..."
+  GOOS=linux GOARCH=386 CGO_ENABLED=1 go install std
 
-echo "Bootstrapping linux/mips64..."
-GOOS=linux GOARCH=mips64 CGO_ENABLED=1 CC=mips64-linux-gnuabi64-gcc-6 go install std
+  echo "Bootstrapping linux/arm64..."
+  GOOS=linux GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc-6 go install std
 
-echo "Bootstrapping linux/mips64le..."
-GOOS=linux GOARCH=mips64le CGO_ENABLED=1 CC=mips64el-linux-gnuabi64-gcc-6 go install std
+  echo "Bootstrapping linux/mips64..."
+  GOOS=linux GOARCH=mips64 CGO_ENABLED=1 CC=mips64-linux-gnuabi64-gcc-6 go install std
 
-echo "Bootstrapping linux/mips..."
-GOOS=linux GOARCH=mips CGO_ENABLED=1 CC=mips-linux-gnu-gcc-6 go install std
+  echo "Bootstrapping linux/mips64le..."
+  GOOS=linux GOARCH=mips64le CGO_ENABLED=1 CC=mips64el-linux-gnuabi64-gcc-6 go install std
 
-echo "Bootstrapping linux/mipsle..."
-GOOS=linux GOARCH=mipsle CGO_ENABLED=1 CC=mipsel-linux-gnu-gcc-6 go install std
+  echo "Bootstrapping linux/mips..."
+  GOOS=linux GOARCH=mips CGO_ENABLED=1 CC=mips-linux-gnu-gcc-6 go install std
 
-echo "Bootstrapping linux/ppc64le..."
-GOOS=linux GOARCH=ppc64le CGO_ENABLED=1 CC=powerpc64le-linux-gnu-gcc-6 go install std
+  echo "Bootstrapping linux/mipsle..."
+  GOOS=linux GOARCH=mipsle CGO_ENABLED=1 CC=mipsel-linux-gnu-gcc-6 go install std
 
-echo "Bootstrapping linux/s390x..."
-GOOS=linux GOARCH=s390x CGO_ENABLED=1 CC=s390x-linux-gnu-gcc-6 go install std
+  echo "Bootstrapping linux/ppc64le..."
+  GOOS=linux GOARCH=ppc64le CGO_ENABLED=1 CC=powerpc64le-linux-gnu-gcc-6 go install std
 
-echo "Bootstrapping linux/riscv64..."
-GOOS=linux GOARCH=riscv64 CGO_ENABLED=1 CC=riscv64-linux-gnu-gcc-8 go install std
+  echo "Bootstrapping linux/s390x..."
+  GOOS=linux GOARCH=s390x CGO_ENABLED=1 CC=s390x-linux-gnu-gcc-6 go install std
 
-echo "Bootstrapping windows/amd64..."
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go install std
+  echo "Bootstrapping linux/riscv64..."
+  GOOS=linux GOARCH=riscv64 CGO_ENABLED=1 CC=riscv64-linux-gnu-gcc-8 go install std
 
-echo "Bootstrapping windows/386..."
-GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go install std
+  echo "Bootstrapping windows/amd64..."
+  GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go install std
 
-echo "Bootstrapping freebsd/amd64..."
-GOOS=freebsd GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-pc-freebsd12-gcc go install std
+  echo "Bootstrapping windows/386..."
+  GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go install std
 
-echo "Bootstrapping darwin/amd64..."
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 CC=o64-clang go install std
+  echo "Bootstrapping freebsd/amd64..."
+  GOOS=freebsd GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-pc-freebsd12-gcc go install std
 
-echo "Bootstrapping darwin/arm64..."
-GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 CC=o64-clang go install std
+  echo "Bootstrapping darwin/amd64..."
+  GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 CC=o64-clang go install std
 
-echo "Bootstrapping linux/arm-5..."
-CC=arm-linux-gnueabi-gcc-6 GOOS=linux GOARCH=arm GOARM=5 CGO_ENABLED=1 CGO_CFLAGS="-march=armv5" CGO_CXXFLAGS="-march=armv5" go install std
-if [ -d "/usr/local/go/pkg/linux_arm" ]; then
-  mv /usr/local/go/pkg/linux_arm /usr/local/go/pkg/linux_arm-5
-fi
+  echo "Bootstrapping darwin/arm64..."
+  GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 CC=o64-clang go install std
 
-echo "Bootstrapping linux/arm-6..."
-CC=arm-linux-gnueabi-gcc-6 GOOS=linux GOARCH=arm GOARM=6 CGO_ENABLED=1 CGO_CFLAGS="-march=armv6" CGO_CXXFLAGS="-march=armv6" go install std
-if [ -d "/usr/local/go/pkg/linux_arm" ]; then
-  mv /usr/local/go/pkg/linux_arm /usr/local/go/pkg/linux_arm-6
-fi
+  echo "Bootstrapping linux/arm-5..."
+  CC=arm-linux-gnueabi-gcc-6 GOOS=linux GOARCH=arm GOARM=5 CGO_ENABLED=1 CGO_CFLAGS="-march=armv5" CGO_CXXFLAGS="-march=armv5" go install std
+  if [ -d "/usr/local/go/pkg/linux_arm" ]; then
+    mv /usr/local/go/pkg/linux_arm /usr/local/go/pkg/linux_arm-5
+  fi
 
-echo "Bootstrapping linux/arm-7..."
-CC=arm-linux-gnueabihf-gcc-6 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CGO_CFLAGS="-march=armv7-a" CGO_CXXFLAGS="-march=armv7-a" go install std
-if [ -d "/usr/local/go/pkg/linux_arm" ]; then
-  mv /usr/local/go/pkg/linux_arm /usr/local/go/pkg/linux_arm-7
+  echo "Bootstrapping linux/arm-6..."
+  CC=arm-linux-gnueabi-gcc-6 GOOS=linux GOARCH=arm GOARM=6 CGO_ENABLED=1 CGO_CFLAGS="-march=armv6" CGO_CXXFLAGS="-march=armv6" go install std
+  if [ -d "/usr/local/go/pkg/linux_arm" ]; then
+    mv /usr/local/go/pkg/linux_arm /usr/local/go/pkg/linux_arm-6
+  fi
+
+  echo "Bootstrapping linux/arm-7..."
+  CC=arm-linux-gnueabihf-gcc-6 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CGO_CFLAGS="-march=armv7-a" CGO_CXXFLAGS="-march=armv7-a" go install std
+  if [ -d "/usr/local/go/pkg/linux_arm" ]; then
+    mv /usr/local/go/pkg/linux_arm /usr/local/go/pkg/linux_arm-7
+  fi
+else
+  echo "Bootstrapping is no longer needed for go 1.20+"
 fi
